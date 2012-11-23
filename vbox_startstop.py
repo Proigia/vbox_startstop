6c97aefb84c60c7c5b2b3955689ab1245f508e14
Dolf Andringa <dandringa@gmail.com>
2012-11-23 12:31:20 +0100

    changed the license from GPL to MIT

--- vbox_startstop.py
+++ vbox_startstop.py
@@ -2,13 +2,34 @@
 """Stop all running virtualbox machines, or start the ones that were previously stopped. For more info, see the usage method (or call 'vbox_startstop.py help')"""
 
 __author__      = "Dolf Andringa"
-__copyright__   = "Copyright 2012, Dolf Andringa"
+__copyright__   = "Copyright (c) 2012, Dolf Andringa"
 
-__license__     = "GPL"
+__license__     = "MIT License"
 __version__     = "1.0"
 __maintainer__  = "Dolf Andringa"
 __email__       = "dolfandringa@gmail.com"
 __status__      = "Production"
+
+__licensetext__ = """Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE."""
 
 from vboxapi import VirtualBoxManager
 import sys, getopt, os, time, traceback
